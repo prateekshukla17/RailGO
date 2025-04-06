1ffc6ea9a79c827c0399929d0bd9bc24a6b1a7c3
prateekshukla17 <4944.prateek@gmail.com>
2025-04-06 18:58:32 +0530

    added trains route and get_trains controller to implement the get trains from-to- statio logic

---
+++ controllers/get_trains.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const getTrains = async function (from, to, date) {
+  const url = `https://irctc1.p.rapidapi.com/api/v3/trainBetweenStations?fromStationCode=${from}&toStationCode=NDLS${to}&dateOfJourney=${date}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': process.env.xrapid_apikey,
+      'x-rapidapi-host': 'irctc1.p.rapidapi.com',
+    },
+  };
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const fetched_data = await response.json();
+    const trains = fetched_data.map((train) => ({
+      train_name: train.train_name,
+      train_number: train.train_number,
+      duration: train.duration,
+      from_std: train.from_std,
+      to_std: train.to_std,
+    }));
+
+    return trains;
+  } catch (e) {
+    console.error('Error Fetching the data:', e.message);
+  }
+};
+
+module.exports = {
+  getTrains: getTrains,
+};
