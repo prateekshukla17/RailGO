6862d07c9d1461df9809031df0ba6d7374be62c7
prateekshukla17 <4944.prateek@gmail.com>
2025-04-06 17:02:37 +0530

    added auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,23 @@
+const { decrypt } = require('dotenv');
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const JWT_SECRET = process.env.jwt;
+
+app.use(express.json());
+
+const auth = function (req, res, next) {
+  const token = req.header.token;
+
+  const decodedData = jwt.verify(token, JWT_SECRET);
+
+  if (decodedData) {
+    // req.id = decodedData._id;
+    next();
+  } else {
+    res.status(403).json({
+      message: 'Incorrect Credentialss',
+    });
+  }
+};
