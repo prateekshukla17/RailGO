082d13cfd359072210d3ff4a6de387c3088c8819
prateekshukla17 <4944.prateek@gmail.com>
2025-04-06 22:53:04 +0530

    added db schema for pnr service

---
+++ dbschema/pnr_model.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { number, string } = require('zod');
+const Schema = mongoose.Schema;
+const Object_Id = mongoose.Types.ObjectId;
+
+const pnr_Schema = new Schema({
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  pnr: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  trainNo: String,
+  trainName: String,
+  from: String,
+  departureTime: String,
+  arrivalTime: String,
+  subscribedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const pnrModel = mongoose.model('PnrSubscription', pnr_Schema);
+module.exports = {
+  pnrModel: pnrModel,
+};
