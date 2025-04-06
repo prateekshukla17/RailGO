0dbdfc8b5a55ecb29d6d882491ddfe46324e0062
prateekshukla17 <4944.prateek@gmail.com>
2025-04-06 16:34:56 +0530

    completed /api/v1/user/signup endpoint

--- dbschema/user_model.js
+++ dbschema/user_model.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 const Object_Id = mongoose.Types.ObjectId;
 
@@ -10,10 +9,7 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  password: { type: String, required: true },
 });
 
 const userModel = mongoose.model('User', userSchema);
--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,16 @@ const express = require('express');
 const { Router } = require('express');
 const userRouter = Router();
 const { z } = require('zod');
-
+const bcrypt = require('bcrypt');
+const { userModel } = require('../dbschema/user_model');
+const { connectDB } = require('../dbschema/connection');
+connectDB();
 userRouter.use(express.json());
 
 userRouter.post('/signup', async function (req, res) {
   const requiredBody = z.object({
     email: z.string().min(3).max(100).email(),
-    name: z.string().min(1).max(20),
+    username: z.string().min(1).max(20),
     password: z.string().min(1).max(20),
   });
 
@@ -18,6 +21,35 @@ userRouter.post('/signup', async function (req, res) {
     res.status(403).json({
       message: 'Incorrect Format',
       error: parseData.error.format(),
+    });
+  }
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+
+  try {
+    const existingUser = await userModel.findOne({ email: email });
+    if (existingUser) {
+      return res.status(403).json({
+        message: 'Email already exists',
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 5);
+
+    await userModel.create({
+      username: username,
+      password: hashedPassword,
+      email: email,
+    });
+
+    res.status(200).json({
+      message: 'You have been signed up',
+    });
+  } catch (e) {
+    console.log('Error while Signing Up:', e);
+    res.status(500).json({
+      message: 'Internal Server Error',
     });
   }
 });
